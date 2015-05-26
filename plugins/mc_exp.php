50bb7eb3e21c3da17736966788700ab6793ab581
Miks Zvirbulis <mikszvirbulis@yahoo.com>
2015-05-26 23:09:09 +0100

    mc_exp

---
+++ plugins/mc_exp.php
@@ -0,0 +1,251 @@
+<?php
+if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != "xmlhttprequest") die("Ajax Only!");
+$p = basename(__FILE__, ".php");
+defined("config_present") or require "../config.inc.php";
+defined("mc_config_present") or require "../config.minecraft.php";
+in_array($p, $c['sms']['plugins']) or die(baltsms::alert("Spraudnis nav ievadīts atļauto spraudņu sarakstā!", "danger"));
+/*
+-----------------------------------------------------
+    Minecraft EXP spraudņa konfigurācija
+-----------------------------------------------------
+*/
+
+/*
+    EXP pircēju tabulas nosaukums
+*/
+$c[$p]['db']['table'] = "baltsms_mc_exp";
+
+/*
+    Vai uzrādīt pircēju sarakstu - jā/nē - true/false
+*/
+$c[$p]['sms']['buyers'] = true;
+
+/*
+    EXP iedošanas komanda. Pēc noklusējuma, pievienota Essentials komanda
+*/
+$c[$p]['commands']['giveEXP'] = "exp give <NICKNAME> <AMOUNT>";
+
+$c[$p]['prices'] = array(
+    "skyblock" => array(
+    	25 => 10,
+    	50 => 30,
+    	75 => 100
+    ),
+    "test" => array(
+    	50 => 5,
+    	100 => 15,
+    	150 => 50
+    )
+);
+
+$c['lang'][$p]['lv'] = array(
+    "instructions" => "Lai iegādātos <LENGTH> EXP par <PRICE> EUR, sūti kodu <b><KEYWORD><CODE></b> uz <b><NUMBER></b>, lai saņemtu atslēgas kodu!",
+	# Kļūdas
+    "error_empty_nickname" => "Ievadi savu spēlētāja vārdu!",
+    "error_empty_server" => "Izvēlies serveri!",
+    "error_empty_price" => "Izvēlies cenu!",
+    "error_empty_code" => "Ievadi atslēgas kodu!",
+    "error_invalid_code" => "Atslēgas kods nav pareizi sastādīts!",
+    "error_price_not_listed" => "Izvēlētā cena nav atrasta priekš izvēlētā servera!",
+    "exp_purchased" => "EXP veiksmīgi iegādāti. Lai jauka spēlēšana!",
+	# Forma
+    "form_price" => "Cena",
+    "form_code" => "Atslēgas kods",
+    "form_player_name" => "Spēlētājs",
+    "form_server" => "Serveris",
+    "form_select_server" => "Izvēlies serveri",
+    "form_price" => "Cena",
+    "form_select_price" => "Izvēlies cenu",
+    "form_unlock_code" => "Atslēgas kods",
+    "form_buy" => "Pirkt",
+	# Tabula
+    "table_nickname" => "Spēlētājs",
+    "table_server" => "Serveris",
+    "table_exp" => "EXP",
+    "table_date" => "Datums",
+    "table_no_buyers" => "Neviens vēl nav iegādājies EXP. Varbūt vēlies būt pirmais?"
+);
+
+$c['lang'][$p]['en'] = array(
+	"instructions" => "To purchase <LENGTH> EXP for <PRICE> EUR, send the following code: <b><KEYWORD><CODE></b> to <b><NUMBER></b> to receive an unclock code!",
+	# Kļūdas
+	"error_empty_nickname" => "Enter your nickname!",
+	"error_empty_server" => "Select the server!",
+	"error_empty_price" => "Select the price!",
+	"error_empty_code" => "Enter the unlock code!",
+	"error_invalid_code" => "The format of the unlock code is not valid!",
+	"error_price_not_listed" => "The selected price has not been found for the selected server!",
+	"exp_purchased" => "EXP was purchased successfully. Have fun!",
+	# Forma
+	"form_price" => "Price",
+	"form_code" => "Unlock code",
+	"form_player_name" => "Player",
+	"form_server" => "Server",
+	"form_select_server" => "Select server",
+	"form_price" => "Price",
+	"form_select_price" => "Select price",
+	"form_unlock_code" => "Unlock code",
+	"form_buy" => "Buy",
+	# Tabula
+	"table_nickname" => "Player",
+    "table_server" => "Server",
+    "table_exp" => "EXP",
+    "table_date" => "Date",
+    "table_no_buyers" => "No one has bought any EXP yet. Would you like to be the first?"
+);
+/*
+-----------------------------------------------------
+    Minecraft EXP spraudņa konfigurācija
+-----------------------------------------------------
+*/
+$db = new db($mc['db']['host'], $mc['db']['username'], $mc['db']['password'], $mc['db']['database']);
+if($db->connected === false) die(baltsms::alert("Nevar izveidot savienojumu ar MySQL serveri. Pārbaudi norādītos pieejas datus!", "danger"));
+$lang[$p] = $c['lang'][$p][$c['page']['lang_personal']];
+?>
+<?php if(isset($_POST['nickname'])): ?>
+	<?php
+	$errors = array();
+
+	if(empty($_POST['nickname'])){
+		$errors[] = $lang[$p]['error_empty_nickname'];
+	}
+
+	if(empty($_POST['server'])){
+		$errors[] = $lang[$p]['error_empty_server'];
+	}
+
+	if(empty($_POST['price']) AND !empty($_POST['server'])){
+		$errors[] = $lang[$p]['error_empty_price'];
+	}else{
+		if(!isset($c[$p]['prices'][$_POST['server']][$_POST['price']])){
+			$errors[] = $lang[$p]['error_price_not_listed'];
+		}
+	}
+
+	if(empty($_POST['code'])){
+		$errors[] = $lang[$p]['error_empty_code'];
+	}else{
+		if(strlen($_POST['code']) != 9 OR is_numeric($_POST['code']) === false){
+			$errors[] = $lang[$p]['error_invalid_code'];
+		}
+	}
+
+	if(count($errors) > 0){
+		foreach($errors as $error){
+			echo baltsms::alert($error, "danger");
+		}
+	}else{
+		$baltsms = new baltsms();
+		$baltsms->setPrice($_POST['price']);
+		$baltsms->setCode($_POST['code']);
+		$baltsms->sendRequest();
+		if($baltsms->getResponse() === true){
+			$db->insert("INSERT INTO `" . $c[$p]['db']['table'] . "` (`nickname`, `server`, `exp`, `time`) VALUES (?, ?, ?, ?)", array(
+				$_POST['nickname'],
+				$_POST['server'],
+				$c[$p]['prices'][$_POST['server']][$_POST['price']],
+				time()
+				));
+
+			$giveEXP = str_replace(
+				array("<NICKNAME>", "<AMOUNT>"),
+				array($_POST['nickname'], $c[$p]['prices'][$_POST['server']][$_POST['price']]),
+				$c[$p]['commands']['giveEXP']
+				);
+			$mc['rcon'][$_POST['server']]->send_command($giveEXP);
+			echo baltsms::alert($lang[$p]['exp_purchased'], "success");
+			?>
+			<script type="text/javascript">
+				setTimeout(function(){
+					loadPlugin('<?php echo $p; ?>');
+				}, 3000);
+			</script>
+			<?php
+		}else{
+			echo $baltsms->getResponse();
+		}
+	}
+	?>
+<?php else: ?>
+	<?php
+	if($db->tableExists($c[$p]['db']['table']) === false) echo baltsms::alert("Tabula netika atrasta datubāzē. Tā tika izveidota automātiski ar nosaukumu, kas norādīts konfigurācijā!", "success");
+	if($db->tableExists($c[$p]['db']['table']) === false) echo baltsms::createTable($p, $c[$p]['db']['table']);
+	?>
+	<form class="form-horizontal" method="POST" id="<?php echo $p; ?>">
+		<div class="alert alert-info" id="instructions" style="display: none;"><?php echo baltsms::instructionTemplate($lang[$p]['instructions'], array("price" => baltsms::returnPrice(0), "code" => 0, "length" => 0)); ?></div>
+		<div id="alerts"></div>
+		<div class="form-group">
+			<label for="nickname" class="col-sm-2 control-label"><?php echo $lang[$p]['form_player_name']; ?></label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" name="nickname" placeholder="<?php echo $lang[$p]['form_player_name']; ?>">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="price" class="col-sm-2 control-label"><?php echo $lang[$p]['form_server']; ?></label>
+			<div class="col-sm-10">
+				<select class="form-control" name="server" onChange="listPrices('none', this.value)">
+					<option selected disabled><?php echo $lang[$p]['form_server']; ?></option>
+					<?php foreach($mc['servers'] as $type => $data): ?>
+						<?php if($data->show !== false): ?>
+							<option value="<?php echo $type; ?>"><?php echo $data->title; ?></option>
+						<?php endif; ?>
+					<?php endforeach; ?>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="price" class="col-sm-2 control-label"><?php echo $lang[$p]['form_price']; ?></label>
+			<div class="col-sm-10">
+				<select class="form-control" id="prices">
+					<option selected disabled><?php echo $lang[$p]['form_select_server']; ?></option>
+				</select>
+				<?php foreach($c[$p]['prices'] as $server => $prices): ?>
+					<select class="form-control prices" name="price" id="none-<?php echo $server; ?>-prices" style="display: none;" onChange="changePrice(this)" disabled>
+						<option selected disabled><?php echo $lang[$p]['form_select_price']; ?></option>
+						<?php foreach($prices as $price_code => $exp): ?>
+							<option value="<?php echo $price_code; ?>" data-length="<?php echo $exp; ?>"><?php echo $exp; ?> EXP - <?php echo baltsms::returnPrice($price_code); ?> EUR</option>
+						<?php endforeach; ?>
+					</select>
+				<?php endforeach;  ?>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="name" class="col-sm-2 control-label"><?php echo $lang[$p]['form_unlock_code']; ?></label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" name="code" placeholder="<?php echo $lang[$p]['form_unlock_code']; ?>" maxlength="9" autocomplete="off">
+			</div>
+		</div>
+		<div class="form-group">
+			<div id="baltsms-form-button">
+				<button type="submit" class="btn btn-primary"><?php echo $lang[$p]['form_buy']; ?></button>
+			</div>
+		</div>
+	</form>
+	<?php if($c[$p]['sms']['buyers'] === true): ?>
+		<table class="table table-bordered">
+			<thead>
+				<th><?php echo $lang[$p]['table_nickname']; ?></th>
+				<th><?php echo $lang[$p]['table_server']; ?></th>
+				<th><?php echo $lang[$p]['table_date']; ?></th>
+				<th><?php echo $lang[$p]['table_exp']; ?></th>
+			</thead>
+			<tbody>
+				<?php $buyers = $db->fetchAll("SELECT * FROM `" . $c[$p]['db']['table'] . "` ORDER BY `time` DESC"); ?>
+				<?php if(empty($buyers)): ?>
+					<tr>
+						<td colspan="4"><?php echo $lang[$p]['table_no_buyers']; ?></td>
+					</tr>
+				<?php else: ?>
+					<?php foreach($buyers as $buyer): ?>
+						<tr>
+							<td><?php echo $buyer['nickname']; ?></td>
+							<td><?php echo $mc['servers'][$buyer['server']]->title; ?></td>
+							<td><?php echo date("d/m/y H:i", $buyer['time']); ?></td>
+							<td><?php echo $buyer['exp']; ?> EXP</td>
+						</tr>
+					<?php endforeach; ?>
+				<?php endif; ?>
+			</tbody>
+		</table>
+	<?php endif; ?>
+<?php endif; ?>
